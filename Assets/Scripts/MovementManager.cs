ad08d2181bbec4e3e9c39d594e76f755db5f4cad
theflyinhawaiian <49086050+theflyinhawaiian@users.noreply.github.com>
2024-01-27 15:59:35 -0500

    Player movement (#1)

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    public float jumpForce;
+    public float playerGravity;
+
+    CharacterController ctrl;
+    Vector3 playerVelocity = Vector3.zero;
+
+    bool canJump = true;
+
+    void Start()
+    {
+        ctrl = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        if (ctrl.isGrounded && !canJump) {
+            playerVelocity.y = 0;
+            canJump = true;
+        }
+
+        if (canJump && Input.GetButton("Jump")) {
+            playerVelocity.y = jumpForce;
+            canJump = false;
+        }
+
+        var vert = Input.GetAxis("Vertical");
+        var hor = Input.GetAxis("Horizontal");
+
+        var move = new Vector3(-hor, 0 , -vert);
+
+        ctrl.Move(move);
+
+        if (!ctrl.isGrounded) {
+            playerVelocity.y += playerGravity * Time.fixedDeltaTime;
+        }
+
+        ctrl.Move(playerVelocity);
+    }
+}
