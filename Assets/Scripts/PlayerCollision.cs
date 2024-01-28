1edab6ed31fcf3de753be26a2b9881ecbe3585ad
Mike McGhen <mcghenmike@gmail.com>
2024-01-28 18:37:15 -0500

    BMan seems good enough

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -11,7 +11,7 @@ public class PlayerCollision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playertransform = GetComponent<Transform>();
+        //playertransform = GetComponent<Transform>();
     }
 
     private void OnCollisionEnter(Collision collision)
