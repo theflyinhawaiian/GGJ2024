b07e4c576ae82b5116dd10861bd93288191c65cd
Mike McGhen <mcghenmike@gmail.com>
2024-01-28 14:17:43 -0500

    Integrated Animation Manager Properly with Movement Manager

--- Assets/Scripts/AnimationMovementManager.cs
+++ Assets/Scripts/AnimationMovementManager.cs
@@ -6,70 +6,75 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 5.0f;
     public float sprintSpeed = 10.0f;
     public float jumpHeight = 2.0f;
-    public float rotationSpeed = 5.0f; // Speed of rotation
+    public float rotationSpeed = 5.0f; 
     public Transform playerCamera;
     public Animator animator;
 
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool isGrounded;
+    private bool isGrounded = true;
     private float gravityValue = -9.81f;
     private float currentSpeed;
+
+    private MovementManager moveManager;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        moveManager = GetComponent<MovementManager>();
+        moveManager.JumpEvent += (obj, e) =>
+        {
+            animator.SetTrigger("Jump");
+            Debug.Log("Jump Triggered!"); // Debugging line
+        };
     }
 
     private void Update()
     {
-        isGrounded = controller.isGrounded;
-        if (isGrounded && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+        //isGrounded = controller.isGrounded;
+        //Debug.Log($"Is Grounded: {isGrounded}, Velocity Y: {playerVelocity.y}");
 
-        // Get input and calculate movement direction in relation to camera
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 forward = playerCamera.forward;
-        Vector3 right = playerCamera.right;
-        forward.y = 0;
-        right.y = 0;
-        forward.Normalize();
-        right.Normalize();
-        Vector3 moveDirection = forward * move.z + right * move.x;
+        //if (isGrounded)
+        //{
+        //    if (playerVelocity.y < 0)
+        //    {
+        //        playerVelocity.y = 0f;
+        //    }
+        //}
 
-        // Check if there is movement to determine if the character should rotate
-        if (moveDirection != Vector3.zero)
-        {
-            // Calculate target rotation based on movement direction
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-            // Smoothly rotate towards the target rotation over time
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //Vector3 forward = playerCamera.forward;
+        //Vector3 right = playerCamera.right;
+        //forward.y = 0;
+        //right.y = 0;
+        //forward.Normalize();
+        //right.Normalize();
+        //Vector3 moveDirection = forward * move.z + right * move.x;
 
-        // Determine if the character is sprinting
+        //if (moveDirection != Vector3.zero)
+        //{
+        //    Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //}
+
         bool isSprinting = Input.GetKey(KeyCode.LeftShift);
         currentSpeed = isSprinting ? sprintSpeed : walkSpeed;
 
-        // Move the character
-        controller.Move(currentSpeed * Time.deltaTime * moveDirection);
-
-        // Update animator parameters
+        // Set animator parameters
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
-        animator.SetFloat("Speed", moveDirection.magnitude);
-        animator.SetBool("IsJumping", !isGrounded);
+        animator.SetFloat("Speed", moveManager.Speed);
         animator.SetBool("IsSprinting", isSprinting);
 
-        // Jumping logic
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
+        //if (Input.GetButton("Jump") && isGrounded)
+        //{
+        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        //    animator.SetTrigger("Jump");
+        //    Debug.Log("Jump Triggered!"); // Debugging line
+        //}
 
-        // Apply gravity
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        //playerVelocity.y += gravityValue * Time.deltaTime;
+        //controller.Move(playerVelocity * Time.deltaTime);
+        //controller.Move(currentSpeed * Time.deltaTime * moveDirection);
     }
 }
